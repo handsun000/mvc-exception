600d0734f3762f9bd7f755222d330c44e7125108
son jin yeong <handsun000@gmail.com>
2021-11-22 16:45:51 +0900

    ServletExController

--- src/main/java/hello/exception/servlet/ServletExController.java
+++ src/main/java/hello/exception/servlet/ServletExController.java
@@ -13,12 +13,18 @@ public class ServletExController {
 
     @GetMapping("/error-ex")
     public void errorEx() {
-        throw new RuntimeException("에외 발생!");
+        log.info("발생");
+        throw new RuntimeException("예외 발생!");
     }
 
     @GetMapping("error-404")
     public void error404(HttpServletResponse response) throws IOException {
         response.sendError(404, "404 오류!");
+    }
+
+    @GetMapping("error-400")
+    public void error400(HttpServletResponse response) throws IOException {
+        response.sendError(400, "400 오류!");
     }
 
     @GetMapping("error-500")
